6b6821562f52620545106519320cfd147127c159
Tim Schottler <tim@tim12.com>
2019-09-27 12:49:14 -0500

    Updates

---
+++ precache-manifest.12f65047123c2ee5651a7d32f4d4ca9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c54be4a7b8e9de672d83bcf94b83e11",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bd8aac93c534852eb488",
+    "url": "/monsters-rolodex/static/css/main.a5f0b982.chunk.css"
+  },
+  {
+    "revision": "abc1ce18aadecde23a06",
+    "url": "/monsters-rolodex/static/js/2.e011650a.chunk.js"
+  },
+  {
+    "revision": "bd8aac93c534852eb488",
+    "url": "/monsters-rolodex/static/js/main.9d01ad9b.chunk.js"
+  },
+  {
+    "revision": "0d0f416e7b5fe29edb74",
+    "url": "/monsters-rolodex/static/js/runtime-main.9ce5b26a.js"
+  }
+]);
